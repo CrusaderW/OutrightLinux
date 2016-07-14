3d6d04f52538339f80ffc131466f20ff9a8cf319
Christian Wirth <wirth@protectivecircle.com>
2016-07-14 16:32:40 +0200

    adds script that configures git

---
+++ scripts/developement/configure_git.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#TODO check if git is installed: if not: do so...
+git config --global user.email "wirth@protectivecircle.com"
+git config --global user.name "Christian Wirth"
+git config --global push.default simple
