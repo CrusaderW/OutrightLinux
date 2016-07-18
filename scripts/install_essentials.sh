45a3ee37e904119afff5f5d6caa6cc164988e99a
Christian Wirth <wirth@protectivecircle.com>
2016-07-18 20:54:20 +0200

    installs seahorse and seahorse-nautilus

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -5,4 +5,11 @@ sudo apt-get install \
 	pidgin \
 	pidgin-otr \
 	#TODO: activate OTR with python script
+	seahorse \
+	seahorse-nautilus \
 	-y
+
+echo "Der Dateimanager (Nautilus) wird nun beendet gestartet um die Inntegration \
+	der Verschluesselungsfunktion durzufuehren."
+read
+sudo nautilus -q
