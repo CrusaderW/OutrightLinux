9ba7dc7f9b54d6a56fd7d846e5dd37195a598ac6
Christian Wirth <wirth@protectivecircle.com>
2016-07-21 09:28:49 +0200

    adds break on instruction-display

--- scripts/development/install_vim.sh
+++ scripts/development/install_vim.sh
@@ -8,3 +8,5 @@ cd ~/.vim/bundle/YouCompleteMe/
 git submodule update --init --recursive
 ./install.sh --clang-completer
 echo "On firt start of vim ececute ':set spell spelllang=de' -> example to download a german dictionary"
+echo "Press enter to continue"
+read
