048854dc93152854fbcf3364a8f325062845ebc8
Christian Wirth <wirth@outrightsolutions.de>
2016-08-27 11:38:57 +0200

    adds Syncthing as service

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -18,4 +18,4 @@ curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
 sudo apt-get update
 sudo apt-get install syncthing -y
-#TODO: Add Syncthing to autostart.
+systemctl --user enable syncthing.service
