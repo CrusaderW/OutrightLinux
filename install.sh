b7cff2729abd3d9d84928a459a5bc80c40c80ffb
Christian Wirth <wirth@protectivecircle.com>
2017-02-14 11:33:28 +0100

    remove TS3

--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@ sudo echo "Strarting OutrightLinux install-script"
 echo "dummy"
 scripts/install_essentials.sh
 
-#Gaming-Stuff
-scripts/gaming/install_teamspeak3.sh
-
 #Development-Stuff
 scripts/development/install_essentials.sh
 #TODO: Make e-mail and name variable
