6d9eb6469965b3a60dec6a7a3ee44a1fe5cf1725
Christian Wirth <wirth@protectivecircle.com>
2018-01-16 10:06:18 +0100

    remove terminal stuff

--- scripts/install_essentials.sh
+++ scripts/install_essentials.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 sudo apt-get install \
-	terminology \
-  silversearcher-ag \
 	vlc \
 	thunderbird \
 	pidgin \
 	pidgin-otr \
 	#TODO: activate OTR with python script
 	-y
-
-gsettings set org.gnome.desktop.default-applications.terminal exec 'terminology'
 
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
 echo "deb http://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
